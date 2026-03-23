3f3b69c294742643e9fe88945f4327c32e7e5df5
Mootjuh <haddouti_m@live.nl>
2026-03-23 20:17:40 +0100

    Release 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sadaqah.kiosk"
         minSdk = 30
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
