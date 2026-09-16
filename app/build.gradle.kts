61e2fe9185632ba671640dc5f40fa31de32b25b9
Mootjuh <haddouti_m@live.nl>
2026-09-16 08:16:43 +0200

    Cut 1.4.0-preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sadaqah.kiosk"
         minSdk = 30
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.3.6-preview"
+        versionCode = 16
+        versionName = "1.4.0-preview"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
